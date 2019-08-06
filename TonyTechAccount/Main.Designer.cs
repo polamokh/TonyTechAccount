671f9bde85eb898ad0a1985b871de0f45c08cf3c
polamokh <pola.mikhail@engineer.com>
2019-08-06 22:19:49 +0200

    Added Main form.

---
+++ TonyTechAccount/Main.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace TonyTechAccount
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonNewAccount = new System.Windows.Forms.Button();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonBackupDatabase = new System.Windows.Forms.Button();
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.ColumnCount = 1;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.Controls.Add(this.buttonBackupDatabase, 0, 3);
+            this.tableLayoutPanelMain.Controls.Add(this.buttonSearch, 0, 2);
+            this.tableLayoutPanelMain.Controls.Add(this.buttonPrint, 0, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.buttonNewAccount, 0, 0);
+            this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 4;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(529, 312);
+            this.tableLayoutPanelMain.TabIndex = 0;
+            // 
+            // buttonNewAccount
+            // 
+            this.buttonNewAccount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonNewAccount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNewAccount.Location = new System.Drawing.Point(3, 3);
+            this.buttonNewAccount.Name = "buttonNewAccount";
+            this.buttonNewAccount.Size = new System.Drawing.Size(523, 72);
+            this.buttonNewAccount.TabIndex = 0;
+            this.buttonNewAccount.Text = "New Account";
+            this.buttonNewAccount.UseVisualStyleBackColor = true;
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPrint.Location = new System.Drawing.Point(3, 81);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(523, 72);
+            this.buttonPrint.TabIndex = 1;
+            this.buttonPrint.Text = "Print";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearch.Location = new System.Drawing.Point(3, 159);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(523, 72);
+            this.buttonSearch.TabIndex = 2;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // buttonBackupDatabase
+            // 
+            this.buttonBackupDatabase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonBackupDatabase.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBackupDatabase.Location = new System.Drawing.Point(3, 237);
+            this.buttonBackupDatabase.Name = "buttonBackupDatabase";
+            this.buttonBackupDatabase.Size = new System.Drawing.Size(523, 72);
+            this.buttonBackupDatabase.TabIndex = 3;
+            this.buttonBackupDatabase.Text = "Backup Database";
+            this.buttonBackupDatabase.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 312);
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.Button buttonBackupDatabase;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.Button buttonNewAccount;
+    }
+}
--- TonyTechAccount/Program.cs
+++ TonyTechAccount/Program.cs
@@ -16,6 +16,7 @@ namespace TonyTechAccount
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
         }
     }
 }
